ca446dd288cabb8cc42ad54f674d70b5cd0ab2c3
Yogesh Deshmukh <sampoorna.mahanti@tcs.com>
2020-05-15 09:35:19 +0530

    Added gettingPangramwithoutSet method

--- src/main/java/com/ds/algo/string/PangramString.java
+++ src/main/java/com/ds/algo/string/PangramString.java
@@ -12,6 +12,10 @@ public class PangramString {
     public static void main(String[] args) {
         String str = "The quick brown fox jumps over the lazy dog";
         System.out.println(checkPangramString(str));
+        System.out.println(checkPangramWithoutSet(str));
+        str = "The quick brown fox jumps over the dog";
+        System.out.println(checkPangramString(str));
+        System.out.println(checkPangramWithoutSet(str));
     }
 
     /*Using Hashing technique - used HashSet Time -> O(n) and Space -> O(1) bcoz always this will be 26 in size*/
@@ -28,5 +32,34 @@ public class PangramString {
             return true;
         else
             return false;
+    }
+
+
+
+    public static boolean checkPangramWithoutSet(String input){
+
+        boolean[] arr = new boolean[26];
+
+        for(int i=0;i<input.length();i++){
+            char temp = input.charAt(i);
+            int index=0;
+            if(temp >= 'A' && temp <='Z'){
+                index = temp -'A';
+            }
+
+            if(temp >= 'a' && temp <= 'z'){
+                index = temp - 'a';
+            }
+
+            arr[index] = true;
+
+        }
+
+        for(boolean val : arr)
+            if(val == false)
+                return false;
+
+        return true;
+
     }
 }
